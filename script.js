8dc8cdbeeda09e426c7c66deff95825297f67025
odinstudent <147262488+nemanjamalin@users.noreply.github.com>
2024-08-07 17:54:28 +0200

    add scoreboard to track scores, add reset button

--- script.js
+++ script.js
@@ -11,20 +11,45 @@ let game = (function(){
     //currentPlayer
     let currentPlayer = playerX;
 
+    //default board template
+    let emptyGameBoard = `<div class="cell" data-cordinate="00"></div>
+            <div class="cell" data-cordinate="01"></div>
+            <div class="cell" data-cordinate="02"></div>
+            <div class="cell" data-cordinate="10"></div>
+            <div class="cell" data-cordinate="11"></div>
+            <div class="cell" data-cordinate="12"></div>
+            <div class="cell" data-cordinate="20"></div>
+            <div class="cell" data-cordinate="21"></div>
+            <div class="cell" data-cordinate="22"></div>`;
+
     //cahce DOM
     const gameContainer = document.querySelector('.container');
     const startingPlayerOptions = document.querySelector('.options');
     const optionX = document.querySelector('.playerX');
     const optionO = document.querySelector('.playerO');
     const nameInput = document.querySelector('.names');
+    const scoreBoard = document.querySelector('.scoreboard');
+    const resetGame = document.querySelector('.resetGame');
 
     //bind events
-   gameContainer.addEventListener('click',renderSymbol);
-   startingPlayerOptions.addEventListener('click',chooseStartingPlayer);
-   nameInput.addEventListener('keydown',setPlayerName);
+    function bindEvents(){
+        gameContainer.addEventListener('click',renderSymbol);
+        startingPlayerOptions.addEventListener('click',chooseStartingPlayer);
+        nameInput.addEventListener('keyup',setPlayerName);
+        resetGame.addEventListener('click', restartGame);
+    }   
+    bindEvents();
 
 
    //events
+    function restartGame(){
+        gameContainer.innerHTML = emptyGameBoard;
+        playerX.clearPairs();
+        playerO.clearPairs();
+        highlightChosenPlayer(currentPlayer.getSymbol());
+        bindEvents();
+    }
+
     function renderSymbol(event){
         startingPlayerOptions.removeEventListener("click", chooseStartingPlayer); // once game started (symbol is placed) prevent players from changing current player on their own
         const cell = event.target;
@@ -39,7 +64,11 @@ let game = (function(){
 
     function chooseStartingPlayer(event){
         let option = event.target;
-        if(option.innerHTML === "X"){
+        highlightChosenPlayer(option.innerHTML);
+    }
+
+    function highlightChosenPlayer(option){
+        if(option === "X"){
             optionX.classList.add('active');
             optionO.classList.remove('active');
             currentPlayer = playerX;
@@ -58,26 +87,29 @@ let game = (function(){
         if(currentPlayer.isWinner()){
             currentPlayer.increaseScore();
             alert(`${currentPlayer.getName()} is a winner!`);
+            scoreBoard.innerHTML=`
+                Player ${playerX.getName()}: ${playerX.getScore()}
+                <br>
+                Player ${playerO.getName()}: ${playerO.getScore()}                
+                `;
             gameContainer.removeEventListener("click", renderSymbol); 
         }
     }
 
     function setPlayerName(event){
-        const keyCode = event.keyCode;
-        const rightKeyCode = keyCode === 13;
         const playerNameInput = event.target;
         const name = playerNameInput.value || playerNameInput.placeholder;
 
 
-        if(playerNameInput.classList.contains('nameX') && rightKeyCode){
+        if(playerNameInput.classList.contains('nameX')){
             playerX.setName(name);
-            playerNameInput.blur();
-            playerNameInput.classList.add('non-editable');
+            // playerNameInput.blur();
+            // playerNameInput.classList.add('non-editable');
         } 
-        if(playerNameInput.classList.contains('nameO') && rightKeyCode){
+        if(playerNameInput.classList.contains('nameO')){
             playerO.setName(name);
-            playerNameInput.blur();
-            playerNameInput.classList.add('non-editable');
+            // playerNameInput.blur();
+            // playerNameInput.classList.add('non-editable');
         } 
     }
 
@@ -93,6 +125,10 @@ let game = (function(){
         let playerName;
 
         //setters
+
+        const clearPairs = function(){
+            pairs.length = 0;
+        }
 
         const increaseScore = function(){
             score++;
@@ -160,7 +196,7 @@ let game = (function(){
             return false;
         }
 
-        return {getSymbol,insertPair, getScore, increaseScore, isWinner,setName,getName}
+        return {getSymbol,insertPair, getScore, increaseScore, isWinner,setName,getName,clearPairs}
 
     }
 
